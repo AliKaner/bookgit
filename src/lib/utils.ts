0c2382b4b3032914b11037c087a6663ba63782a4
Ali Kaner <alikaner.dev@gmail.com>
2026-03-11 02:51:24 +0300

    feat: add utility functions for combining CSS classes and retrieving the site URL.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@ export function getSiteUrl() {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ??
     process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-    "http://localhost:3000";
+    "https://booktions.com";
     
   url = url.startsWith("http") ? url : `https://${url}`;
   // Ensure trailing slash is removed for consistency if concatenated with /path
